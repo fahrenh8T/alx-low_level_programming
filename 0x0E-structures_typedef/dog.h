05fd12562ecde7acaf1a673c842cdba1942a0aa6
fahrenh8T <raymondrayn98@gmail.com>
2023-04-18 03:06:41 +0300

    gitignore, dog.h and README.md file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - type of struct
+ * @name: name of the dog
+ * @owner: name of the owner
+ * @age: age element
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+
+#endif
