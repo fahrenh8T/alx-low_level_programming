8cd3fdeb97958b66043365513c3e1ea0dd9c1e73
fahrenh8T <raymondrayn98@gmail.com>
2023-04-28 03:59:32 +0300

    0-print_list.c and lists.h file (also included gitignore and README files)

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements in list_t
+ * @lst: linked list node
+ *
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *lst)
+{
+	size_t cnt = 0;
+
+	while (lst != NULL)
+	{
+		if ((*lst).str != NULL)
+			printf("[%d]%s\n", (*lst).len, (*lst).str);
+		else
+			printf("[0] (nil)\n");
+
+		lst = (*lst).next;
+		cnt++;
+	}
+	return (cnt);
+}
