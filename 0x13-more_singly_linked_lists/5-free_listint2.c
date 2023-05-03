eb7dbc9f87e1e008d1349ad8087a47828c863a81
fahrenh8T <raymondrayn98@gmail.com>
2023-05-03 03:50:27 +0300

    5-free_listint2.c updt2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	while (*head != NULL)
+	while (*head != NULL && head != NULL)
 	{
 		node = *head;
 		*head = (*head)->next;
 		free(node);
 	}
+	*head = NULL;
 }
