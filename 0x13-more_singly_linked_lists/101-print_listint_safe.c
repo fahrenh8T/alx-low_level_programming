f67d1bf9aa7081d4cf58b2c54d2ce5b064bccab1
fahrenh8T <raymondrayn98@gmail.com>
2023-05-05 00:56:26 +0300

    modified singly_linked and new bit_manipulation

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,26 +10,35 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *one = head;
-	 const listint_t *two = head->next;
-	size_t all = 0;
+	const listint_t *two = head->next;
 
 	if (head == NULL)
 		exit(98);
 
-	while (two != NULL && two < two->next)
+	while (two && two < one)
 	{
-		printf("[%p]%d\n", (void *)one, one->n);
+		printf("[%p] %d\n", (void *)one, one->n);
 		one = one->next;
-		two = two->next->next;
-		all++;
-	}
-	printf("[%p]%d\n", (void *)one, one->n);
-	all++;
+		two = two->next;
 
-	if (two != NULL)
-	{
-		printf("->[%p]%d\n", (void *)two, two->n);
-		all++;
+		if (two && two < one)
+			two = two->next;
 	}
-	return (all);
+
+	if (two)
+	{
+		printf("[%p] %d\n", (void *)one, one->n);
+		one = one->next;
+		two = two->next;
+		printf("-> [%p] %d\n", (void *)one, one->n);
+		while (two && two >= head && two > one)
+		{
+			printf("[%p] %d\n", (void *)one, one->n);
+			one = one->next;
+			two = two->next;
+		}
+	}
+	printf("[%p] %d\n", (void *)one, one->n);
+
+	return (one - head + print_listint_safe(head));
 }
