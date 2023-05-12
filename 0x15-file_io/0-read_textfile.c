970aaad3cb388bd8cdaeb1cdd8f254e3b3c5316c
fahrenh8T <raymondrayn98@gmail.com>
2023-05-12 23:22:09 +0300

    0-read_textfile.cfile updt2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,35 +8,26 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen(filename, "r");
-	char *data = (char *) malloc(letters * sizeof(char));
-	ssize_t data_read = fread(data, sizeof(char), letters, file);
-	ssize_t data_written = write(STDOUT_FILENO, data, data_read);
+	char *data;
+	ssize_t data_read, data_written;
+	int file_des;
 
 	if (filename == NULL)
 		return (0);
 
-	if (data == NULL)
-	{
-		fclose(file);
+	 file_des = open(filename, O_RDWR);
+	 if (file_des == -1)
+	 	return (0);
+	 
+	 data = (char *) malloc(letters * sizeof(char));
+	 if (data == NULL)
 		return (0);
-	}
 
-	if (data_read < 0)
-	{
-		fclose(file);
-		free(data);
-		return (0);
-	}
+	data_read = read(file_des, data, letters);
+	data_written = write(STDOUT_FILENO, data, data_read);
 
-	if (data_written < 0 || data_written != data_read)
-	{
-		fclose(file);
-		free(data);
-		return (0);
-	}
-
-	fclose(file);
 	free(data);
-	return (data_read);
+
+	close(file_des);
+	return (data_written);
 }
